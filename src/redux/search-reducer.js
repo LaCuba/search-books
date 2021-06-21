1d7e9ca37261486f01cee81f4468a088ed6575e4
LaCuba <nikolay.romanov164@gmail.com>
2021-06-21 19:41:25 +0300

    Created store and SearchReducer.

---
+++ src/redux/search-reducer.js
@@ -0,0 +1,34 @@
+
+const initialState = {
+  snippets: {
+    0: { cover: "https://cdn1.ozone.ru/s3/multimedia-w/wc1200/6012799976.jpg",
+    title: "Как не pro*бать свою жизнь",
+    author: "Байер Майк"
+    },
+    1: { cover: "https://cdn1.ozone.ru/s3/multimedia-w/wc1200/6012799976.jpg",
+    title: "Как не pro*бать свою жизнь",
+    author: "Байер Майк"
+    },
+    2: { cover: "https://cdn1.ozone.ru/s3/multimedia-w/wc1200/6012799976.jpg",
+    title: "Как не pro*бать свою жизнь",
+    author: "Байер Майк"
+    },
+    3: { cover: "https://cdn1.ozone.ru/s3/multimedia-w/wc1200/6012799976.jpg",
+    title: "Как не pro*бать свою жизнь",
+    author: "Байер Майк"
+    },
+    4: { cover: "https://cdn1.ozone.ru/s3/multimedia-w/wc1200/6012799976.jpg",
+    title: "Как не pro*бать свою жизнь",
+    author: "Байер Майк"
+    }
+  }
+}
+
+const SearchReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default: 
+      return state
+  }
+}
+
+export default SearchReducer
---
+++ src/redux/store.js
@@ -0,0 +1,11 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import thunkCreator from 'redux-thunk'
+import SearchReducer from './search-reducer'
+
+const reducers = combineReducers({
+  snippets: SearchReducer
+})
+
+const store = createStore(reducers, applyMiddleware(thunkCreator))
+
+export default store
