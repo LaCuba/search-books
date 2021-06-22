dd219a38bcdf7770468a619428ec58afe2fd1d98
LaCuba <nikolay.romanov164@gmail.com>
2021-06-22 13:32:07 +0300

    Made a timer for automatic search.

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -5,20 +5,22 @@ import styles from './Search.module.scss'
 const Search = (props) => {
 
   const [character, setCharacter] = useState('')
-
+  const [timer, setTimer] = useState(0)
+  
   const onSearchChange = (e) => {
     setCharacter(e.currentTarget.value)
+    clearTimeout(timer)
+    setTimer(setTimeout((value) => console.log(value), 1000, e.currentTarget.value))
   }
-
-  const searchBook = () => {
+  
+  const searchBooks = () => {
     console.log(character)
-    setCharacter('')
   }
 
   return <> 
   <div className={styles.panel}>
     <input type="text" placeholder="Search a book." onChange={onSearchChange} value={character} />
-    <button onClick={searchBook}>Search</button>
+    <button onClick={searchBooks}>Search</button>
   </div>
   </>
 }
